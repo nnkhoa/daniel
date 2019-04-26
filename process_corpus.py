465bded215f25d50bfa171655d774f574d98c0b2
nnkhoa <n.nhukhoa2209@gmail.com>
2019-04-26 13:27:22 +0700

    correct spelling, modified code style to match Python convention

--- process_corpus.py
+++ process_corpus.py
@@ -13,9 +13,11 @@ class Struct:
     self.__dict__.update(entries)
 
 def open_utf8(path):
-  f = codecs.open(path,"r", "utf-8")
-  chaine = f.read()
-  f.close()
+  # f = codecs.open(path,"r", "utf-8")
+  # chaine = f.read()
+  # f.close()
+  with codecs.open(path, "r", "utf-8") as f:
+      chaine = f.read()
   return chaine
 
 def  write_output(output_dic, options):
@@ -38,47 +40,62 @@ def prepare_infos(infos, options):
 def  start_detection(options):
   corpus_to_process = json.load(open(options.corpus))
   cpt_proc, cpt_rel = 0, 0
-  output_dic, ressources = {}, {}
+  output_dic, resources = {}, {}
   not_found = []
   has_not_found = False
   abs_path = ""
   print "\n Processing %s documents\n"%str(len(corpus_to_process))
+  
   for id_file, infos in corpus_to_process.iteritems():
-    if os.path.exists(infos["document_path"])==False:
+    if not os.path.exists(infos["document_path"]):
       abs_path = os.path.dirname(os.path.abspath(options.corpus))+"/"
-      if os.path.exists(abs_path + infos["document_path"])==False:
+      if not os.path.exists(abs_path + infos["document_path"]):
         not_found.append(infos["document_path"])
-        if has_not_found==False:
+        if not has_not_found:
           print "Not found : ",infos["document_path"]
           print "Not found either: ",abs_path+infos["document_path"]
           print "  -> the next not found files will be ignored"
           d = raw_input("Press enter to continue")
           has_not_found = True
         continue
-    cpt_proc+=1
-    if abs_path!="":
-      infos["document_path"]=abs_path+infos["document_path"]
+    
+    cpt_proc += 1
+    
+    if abs_path != "":
+      infos["document_path"] = abs_path + infos["document_path"]
+    
     output_dic[id_file] = infos
+    
     if "annotations" in output_dic[id_file]:
       del output_dic[id_file]["annotations"]# for evaluation
+    
     infos = prepare_infos(infos, options)
-    if options.verbose==True:
+    
+    if options.verbose:
       print infos
-    lg = infos["language"]
-    if lg not in ressources:
-      ressources[lg] = get_ressource(lg, options)
+
+    #lg = infos["language"] 
+    if infos["language"]  not in resources:
+      resources[infos["language"] = get_ressource(infos["language"], options)
+    
     o = Struct(**infos)
-    results = process(o, ressources[lg])
-    if o.verbose==True or o.showrelevant==True:
+    results = process(o, resources[infos["language"])
+    
+    if o.verbose or o.showrelevant:
       process_results(results, o)
+    
     if "dis_infos" in results:
-      cpt_rel +=1
+      cpt_rel += 1
+    
     output_dic[id_file]["annotations"] = results["events"]
     output_dic[id_file]["is_clean"] = str(output_dic[id_file]["is_clean"])
-    if cpt_proc%100==0:
+    
+    if cpt_proc%100 == 0:
       print "%s documents processed, %s relevant"%(str(cpt_proc), str(cpt_rel))
-  output_path = write_output(output_dic, options)
-  if len(not_found)>0:
+    
+    output_path = write_output(output_dic, options)
+  
+  if len(not_found) > 0:
     path_not_found = "tmp/files_not_found"
     print "--\n %s files not found\n"%str(len(not_found))
     print " list here : %s\n--"%(path_not_found)
@@ -103,7 +120,7 @@ if __name__=="__main__":
   print "%s docs proc. in %s seconds"%(str(cpt_doc), str(round(end-start, 4)))
   print "  %s relevant documents"%(str(cpt_rel))
   print "  Results written in %s"%output_path
-  if options.evaluate==True:
+  if options.evaluate:
     print "\nEvaluation\n :"
     cmd = "python evaluate.py %s %s"%(options.corpus, output_path)
     print "-->",cmd
