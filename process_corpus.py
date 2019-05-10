e5a536f33264ad3efddfcda7e9a678acfc302369
nnkhoa <n.nhukhoa2209@gmail.com>
2019-05-10 17:04:50 +0200

    modified the checked missing docs section, removed unused/redundant variables, restored original code section and added my thought on it

--- process_corpus.py
+++ process_corpus.py
@@ -42,10 +42,9 @@ def list_docs_not_found(missing_docs):
         print "list here: %s\n--"%(path)
         write_utf8(path, "\n".join(missing_docs))
 
-def skip_missing_doc(is_skipped, abs_path, doc_path):
+def skip_missing_doc(is_skipped, doc_path):
     if not is_skipped:
         print "Not found: ", doc_path
-        print "Not found either: ",  abs_path + doc_path
         print "-> the next not found files will be ignored"
         d = raw_input("Press enter to continue")
         return True
@@ -54,10 +53,9 @@ def skip_missing_doc(is_skipped, abs_path, doc_path):
 # look for doc - if missing, put in list to print later
 def look_for_doc(doc_path, corpus_path, missing_doc):
     if not os.path.exists(doc_path):
-        # check for absolute path also
-        if not os.path.exists(os.path.dirname(os.path.abspath(corpus_path)) + "/" + doc_path):
-            missing_doc.append(doc_path)
-            return False
+        missing_doc.append(doc_path)
+        return False
+    return True
 
 # return absolute path to document if abs_path exists, otherwise doc_path remain
 def check_abs_path(doc_path, corpus_path):
@@ -70,34 +68,34 @@ def start_detection(options):
   cpt_proc, cpt_rel = 0, 0
   output_dic, resources = {}, {}
   missing_docs = []
-  is_found = False
-  abs_path = ""
+  is_skipped = False
   
   print "\n Processing %s documents\n"%str(len(corpus_to_process))
   
   for id_file, infos in corpus_to_process.iteritems(): 
     infos["document_path"] = check_abs_path(infos["document_path"], options.corpus)
-    
-    if not os.path.exists(infos["document_path"]):
-      abs_path = os.path.dirname(os.path.abspath(options.corpus))+"/"
-      if not os.path.exists(abs_path + infos["document_path"]):
-        missing_docs.append(infos["document_path"])
-        is_found = skip_missing_doc(is_found, abs_path, infos["document_path"])
-        # ???????
-        #if not is_found:
-        #  print "Not found : ",infos["document_path"]
-        #  print "Not found either: ",abs_path+infos["document_path"]
-        #  print "  -> the next not found files will be ignored"
-        #  d = raw_input("Press enter to continue")
-        #  is_found = True
-        # ???????
+
+    # seriously though, I don't even know why this part exists and why it is writen like this 
+    # so confusing
+
+    #if os.path.exists(infos["document_path"])==False:
+    #  abs_path = os.path.dirname(os.path.abspath(options.corpus))+"/"
+    #  if os.path.exists(abs_path + infos["document_path"])==False:
+    #    not_found.append(infos["document_path"])
+    # ???????????????????????????
+    #    if has_not_found==False:
+    #      print "Not found : ",infos["document_path"]
+    #      print "Not found either: ",abs_path+infos["document_path"]
+    #      print "  -> the next not found files will be ignored"
+    #      d = raw_input("Press enter to continue")
+    #      has_not_found = True
+    # ???????????????????????????
+
+    if not look_for_doc(infos["document_path"], options.corpus, missing_doc):
+        is_skipped = skip_missing_doc(is_skipped, infos["document_path"])
         continue
     
     cpt_proc += 1
-    
-    #if abs_path != "":
-    #  infos["document_path"] = abs_path + infos["document_path"]
-    
 
     output_dic[id_file] = infos
     
